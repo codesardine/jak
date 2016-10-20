3b5628d61988fc71f0470d59fe8746cfdffb31f1
Vitor Lopes <vmnlop@gmail.com>
2016-10-20 23:08:09 +0100

    add python example

---
+++ examples/myapp.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+try:
+    from j import AK
+    
+except Exception as err:
+    print("Ops something went wrong: " + str(err))
+    
+# 
+# your code here do some stuff.
+#   
+AK.run()
