242dee3a86abfbcc32648ab26f6d253d49eb930c
Vitor Lopes <vlopes@gmail.com>
2018-01-23 18:07:48 +0000

    version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(readme, 'README.rst'), encoding='utf-8') as f:
 
 setup(
 name             = "Jade-Application-Kit",
-version          = "0.a30",
+version          = "0.a32",
 packages         = ["j"],
 url              = "https://codesardine.github.io/Jade-Application-Kit",
 license          = "GPLv2",
